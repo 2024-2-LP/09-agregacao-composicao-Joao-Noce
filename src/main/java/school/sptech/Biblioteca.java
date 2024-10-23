a5795fa4e44bc434ffbe66814789a3582535d205
Joao-Nocee <joao.noce@sptech.school>
2024-10-23 19:42:39 -0300

    Enviado sem erros

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,66 @@
 package school.sptech;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros = new ArrayList<>();
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro != null && livro.getTitulo() != null && !livro.getTitulo().isBlank() && livro.getAutor() != null && !livro.getAutor().isBlank() && livro.getDataPublicacao() != null)
+            livros.add(livro);
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livroAtual = livros.get(i);
+            if (livroAtual.getTitulo().toLowerCase().equals(titulo.toLowerCase())) livros.remove(livroAtual);
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        if (titulo != null && !titulo.isBlank()) {
+            for (Livro livroAtual : livros) {
+                if (livroAtual.getTitulo().toLowerCase().equals(titulo.toLowerCase())) return livroAtual;
+            }
+        }
+        return null;
+    }
+
+    public Integer contarLivros() {
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        List<Livro> livrosAteAno = new ArrayList<>();
+
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livroAtual = livros.get(i);
+            if (livroAtual.getDataPublicacao().getYear() <= ano) livrosAteAno.add(livroAtual);
+        }
+        return livrosAteAno;
+    }
+
+    public List<Livro> retornarTopCincoLivros() {
+        livros.sort(Comparator.comparingDouble(Livro::calcularMediaAvaliacoes).reversed());
+        if (livros.size() >= 5) return livros.subList(0, 5);
+        else return livros;
+    }
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,70 @@
 package school.sptech;
 
-public class Livro {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes = new ArrayList<>();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas) {
+        if (descricao != null && !descricao.isBlank() && qtdEstrelas != null && qtdEstrelas <= 5.0 && qtdEstrelas >= 0.0) {
+            Avaliacao avaliacao = new Avaliacao(descricao, qtdEstrelas);
+            avaliacoes.add(avaliacao);
+        }
+    }
+
+    public Double calcularMediaAvaliacoes() {
+        Double soma = 0.0;
+        if (avaliacoes.isEmpty()) return 0.0;
+        for (Avaliacao avaliacaoAtual : avaliacoes) {
+            soma += avaliacaoAtual.getQtdEstrelas();
+        }
+        return soma/avaliacoes.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", dataPublicacao=" + dataPublicacao +
+                ", avaliacoes=" + avaliacoes +
+                '}';
+    }
 }
